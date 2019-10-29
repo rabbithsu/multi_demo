405ece85ab92132c9162ba92d342756a3b2440cb
alexhsu <alexhsu@fortinet.com>
2019-10-29 11:00:46 +0800

    multi threading+process

--- multi_fun.py
+++ multi_fun.py
@@ -1,22 +1,36 @@
 import threading
 import time
 import sys
+import Queue
+
+
+def job(num, q):
+    print num
+    while True:
+        try:
+            f = q.get(False)
+            a = 0
+            while (a < 100000):
+                a += 1
+                continue
+            q.task_done()
+        except Queue.Empty:
+            print("empty queue.")
+            break
+
+
 
 def multi_start(d):
     worker_num = 1
     if len(sys.argv) == 2:
         worker_num = int(sys.argv[1])
 
-    def job(num):
-        print num
-        a = 0
-        while (a < 100000):
-            a += 1
-            continue
-
+    working_q = Queue.Queue()
+    for i in range(0, 1000):
+        working_q.put(str(i))
     threads = []
     for i in range(worker_num):
-        threads.append(threading.Thread(target = job, args = (i,)))
+        threads.append(threading.Thread(target = job, args = (i,q)))
         threads[i].start()
 
 
