6f19e690b7ce955541202add486842e2bcffdaa4
alexhsu <alexhsu@fortinet.com>
2019-10-29 10:55:40 +0800

    multi threading+process

--- multi_fun.py
+++ multi_fun.py
@@ -2,24 +2,30 @@ import threading
 import time
 import sys
 
-worker_num = 1
-if len(sys.argv) == 2:
-    worker_num = int(sys.argv[1])
+def multi_start(d):
+    worker_num = 1
+    if len(sys.argv) == 2:
+        worker_num = int(sys.argv[1])
 
-def job(num):
-    print num
-    a = 0
-    while (a < 100000):
-        a += 1
-        continue
+    def job(num):
+        print num
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
 
-threads = []
-for i in range(worker_num):
-    threads.append(threading.Thread(target = job, args = (i,)))
-    threads[i].start()
+    threads = []
+    for i in range(worker_num):
+        threads.append(threading.Thread(target = job, args = (i,)))
+        threads[i].start()
 
 
-for i in range(5):
-    threads[i].join()
+    for i in range(5):
+        threads[i].join()
 
-print("Done.")
+    print("Done.")
+if __name__ == '__main__':
+    start = time.time()
+    multi_start(False)
+    time_duration = time.time() - start
+    print "Data process: " + str(time_duration) + "s."
