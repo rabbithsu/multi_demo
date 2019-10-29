97d846d65a18e6cd83f44f54a85664e6aa4c3809
alexhsu <alexhsu@fortinet.com>
2019-10-29 15:34:47 +0800

    multi threading+process

--- multi_fun.py
+++ multi_fun.py
@@ -6,148 +6,94 @@ import Queue
 
 def job(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 
 def job0(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job1(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job2(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job3(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job4(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job5(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job6(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job7(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job8(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
 def job9(num, q):
     print num
-    while True:
-        try:
-            f = q.get(False)
-            a = 0
-            while (a < 100000):
-                a += 1
-                continue
-            q.task_done()
-        except Queue.Empty:
-            print("empty queue.")
-            break
+    while q > 0:
+        a = 0
+        while (a < 100000):
+            a += 1
+            continue
+        q -= 1
+
 
 job_list = [job0, job1, job2, job3, job4, job5, job6, job7, job8, job9 ]
 
@@ -156,9 +102,7 @@ def multi_start(d):
     if len(sys.argv) == 2:
         worker_num = int(sys.argv[1])
 
-    working_q = Queue.Queue()
-    for i in range(0, 1000):
-        working_q.put(str(i))
+    j = 1000 / int(worker_num)
     threads = []
     for i in range(worker_num):
         threads.append(threading.Thread(target = job_list[i](), args = (i,working_q)))
